4658df9954bca28dfe9a0c2fe8fe49191e4f1e85
Joshua Paul <cybernerdie@Joshuas-MacBook-Pro.local>
2023-05-03 00:13:45 +0100

    updated database.sql

--- database.sql
+++ database.sql
@@ -59,3 +59,13 @@ CREATE TABLE order_items (
     FOREIGN KEY (order_id) REFERENCES orders(id),
     FOREIGN KEY (product_id) REFERENCES products(id)
 );
+
+CREATE TABLE order_payments (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    order_id INT UNSIGNED NOT NULL,
+    payment_method VARCHAR(255),
+    amount INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+) 
